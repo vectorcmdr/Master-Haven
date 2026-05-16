f07028531bb1c7dd38cc9ff061bcab3eef11c15c
whrstrsg <whrstrsg@gmail.com>
2026-05-16 18:39:52 -0400

    Update featured.py

--- The_Keeper/cogs/featured.py
+++ The_Keeper/cogs/featured.py
@@ -41,6 +41,8 @@ class FeaturedCog(commands.Cog):
         self.FEATURED_TIME_LIMIT = FEATURED_TIME_LIMIT
         self.log = log_func
         self.count_total_reactions = count_total_reactions_func
+        self.weekly_leaderboard_task = self.create_weekly_leaderboard_task(LEADERBOARD_DAY, LEADERBOARD_TOP)
+        self.weekly_leaderboard_task.start()
         self.FEATURED_MESSAGES = set()
         self.PROCESSING = set()
         self.bootstrapped = False
