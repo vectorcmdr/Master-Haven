b2b20b88028dbc0bd0715d026500604027a6f50b
whrstrsg <whrstrsg@gmail.com>
2026-05-17 22:42:57 -0400

    Update xp_system.py

--- The_Keeper/cogs/xp_system.py
+++ The_Keeper/cogs/xp_system.py
@@ -226,7 +226,7 @@ async def process_message_xp(message):
 
     await add_xp(user_id, role, xp_gain)
 
-    level, leveled_up, dm = add_global_xp(user_id, xp_gain)
+    level, leveled_up, dm = await add_global_xp(user_id, xp_gain)
 
     return xp_gain
 
@@ -265,7 +265,7 @@ async def process_discovery_xp(user_id, discovery_type, channel_id):
 
     await add_xp(user_id, primary_role, xp)
 
-    level, leveled_up, dm = add_global_xp(user_id, xp)
+    level, leveled_up, dm = await add_global_xp(user_id, xp)
 
     if leveled_up:
         member = bot.get_guild(member.guild.id).get_member(user_id) if 'member' in globals() else None
