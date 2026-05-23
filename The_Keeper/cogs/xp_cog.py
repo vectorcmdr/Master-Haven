5f83774b909b1348ee9ae007677d439238748a8b
whrstrsg <whrstrsg@gmail.com>
2026-05-23 12:08:49 -0400

    Update xp_cog.py

--- The_Keeper/cogs/xp_cog.py
+++ The_Keeper/cogs/xp_cog.py
@@ -79,7 +79,7 @@ class XpCog(commands.Cog):
         if len(_message_cache) > 5000:
             _message_cache.clear()
 
-            gained = await process_message_xp(message)
+        gained = await process_message_xp(message)
 
             return gained
 
